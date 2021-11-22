56ee150f8555008484732ba17516a07806880567
DustDev <guillem.palomar.agramunt@gmail.com>
2021-11-22 17:07:25 +0100

    fix: create driver does not allow planned_track

--- lib/common/interfaces/driver/CreateDriver.ts
+++ lib/common/interfaces/driver/CreateDriver.ts
@@ -24,43 +24,6 @@ export interface CreateDriver {
   end_location?: Location;
   provides?: string[];
   status?: DriverStatus;
-  is_locked?: boolean;
-  real_track?: string;
-  planned_track?: string;
-  planned_start_time?: number;
-  planned_end_time?: number;
-
-  price_per_distance?: number;
-  price_per_minute?: number;
-
-  tracing_percent?: number;
-  custom_fields?: Record<string, any>;
-
-  stops?: CreateStop[];
-}
-
-
-export interface CreateDriver {
-  external_id?: string;
-  vehicle_id?: string;
-  label?: string;
-  phone?: string;
-  comments?: string;
-  email?: string;
-  plate?: string;
-  color?: string;
-  vehicle_model?: string;
-  brand?: string;
-  time_window?: [number, number];
-  geo_fences?: string[];
-  max_distance?: number;
-  max_weight?: number;
-  max_volume?: number;
-  max_services?: number;
-  start_location?: Location;
-  end_location?: Location;
-  provides?: string[];
-  status?: DriverStatus;
   is_locked?: boolean;  
   price_per_distance?: number;
   price_per_minute?: number;
